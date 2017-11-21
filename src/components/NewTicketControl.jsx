665cf14e3971cc84398a470f2469f9415784835e
Mark Helt <heltmm@gmail.com>
2017-11-20 18:30:19 -0800

    add state component NewTicketControl

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class NewTicketControl extends React.Component {
+
+ constructor(props) {
+    super(props);
+    this.state = {formVisibleOnPage: false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this); 
+  }
+
+  handleDisplayingNewTicketForm(event){
+    console.log("New ticket button was clicked!");
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render(){
+    return (
+      <button onClick={this.handleDisplayingNewTicketForm}>Request Help</button>
+    );
+  }
+
+}
+
+export default NewTicketControl;
