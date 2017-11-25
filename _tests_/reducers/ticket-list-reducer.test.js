42016e2c7fb36c6f2168ec46b37b21b7fc58fd31
Mark Helt <heltmm@gmail.com>
2017-11-24 20:33:07 -0800

    add jest and reducer

---
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,14 @@
+import ticketList from './../../src/reducers/ticket-list-reducer';
+import constants from './../../src/constants';
+
+describe("Ticket list reducer", () => {
+
+  test('should return equivalent state if no action type is recognized', () => {
+    expect(ticketList([], { type: null })).toEqual([]);
+  });
+
+  test('imported value should match action type string', () => {
+    expect(constants.ADD_TICKET).toEqual('testing testing 123');
+  });
+
+});
